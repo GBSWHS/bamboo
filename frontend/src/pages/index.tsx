1d828681982afae98366e78776ce30336dbc19a8
cth.release <84012697+th-release@users.noreply.github.com>
2023-04-14 23:17:20 +0900

    feat: index page

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React, { useReducer } from 'react'
+import Header from '../components/Header';
+import Loading from '../components/Loading';
+
+class Home extends React.Component {
+  render() {
+    return(
+      <>
+        <Header />
+      </>
+    )
+  }
+}
+
+export default Home;
