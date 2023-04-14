e8df89dd3b989a11fda8018aa7f9f5fb0a717742
cth.release <84012697+th-release@users.noreply.github.com>
2023-04-14 23:21:19 +0900

    feat: import entity

--- backend/src/socket/socket.module.ts
+++ backend/src/socket/socket.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import PostsEntity from 'src/entity/posts.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostsEntity])],
+  exports: [TypeOrmModule],
   providers: [SocketGateway]
 })
 export class SocketModule {}
