10b650698f02d40cf51ea568b44043aad8a1f63e
cth.release <84012697+th-release@users.noreply.github.com>
2023-04-14 23:18:57 +0900

    feat: Loading component

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components';
+import { LoadingProps } from '../utils/interfaces';
+
+class Loading extends React.Component<LoadingProps> {
+  render() {
+    return (
+      <Body className={this.props.isLoading ? "true" : "false"}>
+        <ImageBody>
+          <Image />
+        </ImageBody>
+      </Body>
+    )
+  }
+}
+
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const open = keyframes`
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+`
+
+const close = keyframes`
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+`
+
+const Body = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgb(217, 237, 255);
+  z-index: 1;
+
+  &.true {
+    animation: ${open} .25s forwards;
+  }
+
+  &.false {
+    animation: ${close} .25s forwards;
+  }
+`
+
+const ImageBody = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 120px;
+  height: 120px; 
+`
+  
+const Image = styled.div`
+  background-image: url(/symbol-only.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100%;
+  animation: ${rotate} 30s infinite;
+  opacity: .9;
+`
+
+export default Loading;
