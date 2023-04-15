a0adf006814f116d11e7d5109c012b7fe2ae8ce5
cth.release <84012697+th-release@users.noreply.github.com>
2023-04-15 21:50:54 +0900

    feat: add Events

--- backend/src/socket/socket.gateway.ts
+++ backend/src/socket/socket.gateway.ts
@@ -1,9 +1,14 @@
-import { Logger } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ConnectedSocket, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Namespace, Server, Socket } from 'socket.io'
+import CategoryEntity from 'src/entity/category.entity';
 import PostsEntity from 'src/entity/posts.entity';
 import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Cache } from 'cache-manager';
+import QuestEntity from 'src/entity/quest.entity';
+
 @WebSocketGateway(3002, {
   namespace: "socket",
   cors: true,
@@ -11,11 +16,19 @@ import { Repository } from 'typeorm';
     cors: "*"
   }
 })
+
 export class SocketGateway 
   implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     @InjectRepository(PostsEntity)
-    private postsRepository: Repository<PostsEntity>
+    private postsRepository: Repository<PostsEntity>,
+    @InjectRepository(CategoryEntity)
+    private categoryRepository: Repository<CategoryEntity>,
+    @InjectRepository(QuestEntity)
+    private questRepository: Repository<QuestEntity>,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
+    private configService: ConfigService
   ) {};
     
   @WebSocketServer() nsp: Namespace;
@@ -29,9 +42,69 @@ export class SocketGateway
   handleDisconnect(@ConnectedSocket() socket: Socket) {
     this.logger.log("socket disConnected : " + socket.conn.remoteAddress)
   }
+
+  @SubscribeMessage('getCategory')
+  async getCategory(@ConnectedSocket() socket: Socket) {
+    const category = await this.categoryRepository.find({ 
+      where: {
+        status: true
+      },
+      order: {
+        createdAt: "DESC"
+      }
+    })
+
+    socket.emit('getCategory', category);
+  }
+  
+  @SubscribeMessage('addCategory')
+  async addCategory(@ConnectedSocket() socket: Socket, @MessageBody() payload: { name: string, desc: string, password: string }) {
+    try {
+      if (payload.password !== this.configService.get("ROOT_PASSWORD")) throw("not user")
+      else {
+        await this.categoryRepository.insert({ name: payload.name, desc: payload.desc })
+        socket.emit('addCategory', { success: true })
+      }
+    } catch(err) {
+      socket.emit('addCategory', { success: false })
+    }
+  }
+
+  @SubscribeMessage('addQuest')
+  async addQuest(@ConnectedSocket() socket: Socket, @MessageBody() payload: { quest: string, answer: string, password: string }) {
+    try {
+      if (payload.password !== this.configService.get("ROOT_PASSWORD")) throw("not user")
+      else {
+        await this.questRepository.insert({ quest: payload.quest, answer: payload.answer })
+        socket.emit('addQuest', { success: true })
+      }
+    } catch(err) {
+      socket.emit('addQuest', { success: false })
+    }
+  }
+
+  @SubscribeMessage('getQuest')
+  async getQuest(@ConnectedSocket() socket: Socket) {
+    try {
+      const cnt = await this.questRepository.count();
+      const Quest = await this.questRepository.findOneByOrFail({
+        increment: Math.floor(Math.random() * cnt + 1)
+      })
+
+      socket.emit('getQuest', { 
+        success: true, 
+        Quest: {
+          increment: Quest.increment,
+          quest: Quest.quest
+        }
+      })
+    } catch (err) {
+      socket.emit('getQuest', { success: false })
+    }
+  }
   
   @SubscribeMessage('getPosts')
-  getPosts(@ConnectedSocket() socket: Socket, payload: any) {
+  getPosts(@ConnectedSocket() socket: Socket, @MessageBody() payload: any) {
     return 'Hello world!';
   }
 }
