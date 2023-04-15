ed5f19b63641c7545b1752b440aa871a5677fda2
th-release <cth.release@gmail.com>
2023-04-15 10:47:14 +0900

    feat: add entities

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,16 +5,18 @@ import { SocketGateway } from './socket/socket.gateway';
 import { SocketModule } from './socket/socket.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import PostsEntity from './entity/posts.entity';
+import CategoryEntity from './entity/category.entity';
+import AdminsEntity from './entity/admins.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
       type: "mysql",
       host: "127.0.0.1",
       port: 3306,
-      username: "username",
-      password: "password",
+      username: "cth",
+      password: "xogur38997",
       database: "gbsw-bamboo",
-      entities: [PostsEntity],
+      entities: [PostsEntity, CategoryEntity, AdminsEntity],
       synchronize: true
     }),
     SocketModule
