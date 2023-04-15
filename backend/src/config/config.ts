305a08c42cc09e5a459dee89844ccc524f476623
cth.release <84012697+th-release@users.noreply.github.com>
2023-04-15 21:49:47 +0900

    config: update Env

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -9,6 +9,5 @@ export default () => ({
   REDIS_HOST: process.env.REDIS_HOST,
   REDIS_PORT: parseInt(process.env.REDIS_PORT) || 6379,
   REDIS_TTL: parseInt(process.env.REDIS_TTL) || 100000,
-  SECRET: process.env.SECRET,
   ROOT_PASSWORD: process.env.ROOT_PASSWORD
 })
