e7ccd6a0a8ebbe7e979dd8111d605874fbe7c796
th-release <cth.release@gmail.com>
2023-04-15 11:06:50 +0900

    feat: imports configService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,22 +4,31 @@ import { AppService } from './app.service';
 import { SocketGateway } from './socket/socket.gateway';
 import { SocketModule } from './socket/socket.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigurationModule } from './config/config.module';
 import PostsEntity from './entity/posts.entity';
 import CategoryEntity from './entity/category.entity';
 import AdminsEntity from './entity/admins.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisModule } from './utils/redis';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "127.0.0.1",
-      port: 3306,
-      username: "cth",
-      password: "xogur38997",
-      database: "gbsw-bamboo",
-      entities: [PostsEntity, CategoryEntity, AdminsEntity],
-      synchronize: true
+  imports: [TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "mysql",
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_SCHEMA'),
+        entities: [PostsEntity, CategoryEntity, AdminsEntity],
+        synchronize: configService.get<boolean>('TYPEORM_SYBCHRONIZE')
+      }),
     }),
-    SocketModule
+    SocketModule,
+    redisModule,
+    ConfigurationModule
   ],
   controllers: [AppController],
   providers: [AppService, SocketGateway],
