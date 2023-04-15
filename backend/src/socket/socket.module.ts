9057ef20e33b44de24123f841e5ae7e69dae8361
cth.release <84012697+th-release@users.noreply.github.com>
2023-04-15 21:51:14 +0900

    feat: import entity

--- backend/src/socket/socket.module.ts
+++ backend/src/socket/socket.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { SocketGateway } from './socket.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import PostsEntity from 'src/entity/posts.entity';
+import CategoryEntity from 'src/entity/category.entity';
+import { redisModule } from 'src/utils/redis';
+import QuestEntity from 'src/entity/quest.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostsEntity])],
+  imports: [TypeOrmModule.forFeature([
+    PostsEntity, 
+    CategoryEntity,
+    QuestEntity
+  ]), 
+    redisModule
+  ],
   exports: [TypeOrmModule],
   providers: [SocketGateway]
 })
