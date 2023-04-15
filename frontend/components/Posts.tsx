2897c4c06062d4060fa953ee8f200035da9d1289
cth.release <84012697+th-release@users.noreply.github.com>
2023-04-15 21:32:09 +0900

    feat: Posts Component

---
+++ frontend/components/Posts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Posts() {
+  return (
+    <Body>
+      
+    </Body>
+  )
+}
+
+const Body = styled.div`
+  margin-bottom: 40px;
+`
