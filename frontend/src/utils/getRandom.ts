c7c3ac74165ac7bf550fdedb86c7288a4d2d7690
th-release <cth.release@gmail.com>
2023-04-16 15:33:56 +0900

    feat: getRandom func

---
+++ frontend/src/utils/getRandom.ts
@@ -0,0 +1,27 @@
+function getCharacters(type: string): string {
+  switch (type) {
+    case 'numeric':
+      return '0123456789';
+    case 'lowercase':
+      return 'abcdefghijklmnopqrstuvwxyz';
+    case 'uppercase':
+      return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    case 'alphanumeric':
+    case 'all':
+      return '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    default:
+      throw new Error(`Invalid type: ${type}`);
+  }
+}
+
+export default function getRandom(type: string, length: number): string {
+  let result = '';
+  const characters = getCharacters(type);
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+
+  return result;
+}
