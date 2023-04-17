3e4c06775e64e396cf9a7871ec7359bc8375beb3
th-release <cth.release@gmail.com>
2023-04-17 20:28:01 +0900

    feat: socket.emit utils

---
+++ frontend/src/utils/socket.ts
@@ -0,0 +1,9 @@
+import { Socket } from "socket.io-client";
+import { getCookie } from "./cookies";
+
+export default function emit(socket: Socket, event: string, messageBody?: any | undefined) {
+  socket.emit(event, {
+    ...messageBody,
+    session: getCookie('session_id')
+  })
+}
