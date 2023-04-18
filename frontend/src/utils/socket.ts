7927cc921770c6d24fef9194d85cd7231dd68417
th-release <cth.release@gmail.com>
2023-04-18 14:27:46 +0900

    style: change cookie name

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -4,6 +4,6 @@ import { getCookie } from "./cookies";
 export default function emit(socket: Socket, event: string, messageBody?: any | undefined) {
   socket.emit(event, {
     ...messageBody,
-    session: getCookie('session_id')
+    session: getCookie('session-id')
   })
 }
