d3075503390f3368194af71d3f7dfb419cb42ec1
th-release <cth.release@gmail.com>
2023-04-18 23:27:05 +0900

    feat: add columns

--- backend/src/entity/posts.entity.ts
+++ backend/src/entity/posts.entity.ts
@@ -5,11 +5,17 @@ export default class PostsEntity {
   @PrimaryGeneratedColumn("uuid")
   uuid: string;
 
+  @Column({ name: "num", type: "int", nullable: false, unique: true })
+  num: number;
+
   @Column({ name: "title", type: "varchar", length: 24, nullable: false })
   title: string;
 
   @Column({ name: "desc", type: "text" })
   desc: string;
+
+  @Column({ name: "category", type: "varchar", length: 8, nullable: false })
+  category: string;
 
   @Column({ name: "password", type: "text", nullable: false })
   password: string;
@@ -22,6 +28,9 @@ export default class PostsEntity {
 
   @Column({ name: "remoteAddress", type: "text" })
   remoteAddress: string;
+
+  @Column({ name: "session", type: "varchar", length: 32 })
+  remoteSession: string;
 
   @CreateDateColumn({ name: "createdAt" })
   createdAt: Date;
