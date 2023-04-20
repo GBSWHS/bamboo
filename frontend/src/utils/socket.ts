859ff3d00027fab2fa6ccaf5c76d3df717ac6638
th-release <cth.release@gmail.com>
2023-04-20 10:12:21 +0900

    remove: socket to axios

--- frontend/src/utils/socket.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from "socket.io-client";
-import { getCookie } from "./cookies";
-
-export default function emit(socket: Socket, event: string, messageBody?: any | undefined) {
-  socket.emit(event, {
-    ...messageBody,
-    session: getCookie('session-id')
-  })
-}
