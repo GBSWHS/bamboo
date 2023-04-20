b20ed1a2134d3dacbbca7bdefa82ef89f8e0e25c
cth.release <84012697+th-release@users.noreply.github.com>
2023-04-20 10:18:56 +0900

    feat: add routes

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -10,13 +10,38 @@ export class AppController {
     return this.appService.getStatus();
   }
 
-  @Post('delPost')
-  deletePosts(@Req() req, @Res() res) {
-    this.appService.deletePosts(req, res);
+  @Post('addQuest')
+  addQuest(@Req() req, @Res() res) {
+    return this.appService.addQuest(req, res);
   }
 
-  @Post('posts')
-  addPosts(@Req() req, @Res() res) {
-    this.appService.addPosts(req, res);
+  @Post('addCategory')
+  addCategory(@Req() req, @Res() res) {
+    return this.appService.addCategory(req, res);
+  }
+
+  @Get('getQuest')
+  getQuest(@Req() req, @Res() res) {
+    return this.appService.getQuest(req, res);
+  }
+
+  @Get('getCategory')
+  getCategory(@Req() req, @Res() res) {
+    return this.appService.getCategory(req, res);
+  }
+
+  @Get('getPosts')
+  getPosts(@Req() req, @Res() res) {
+    return this.appService.getPosts(req, res);
+  }
+
+  @Post('delPost')
+  deletePosts(@Req() req, @Res() res) {
+    return this.appService.deletePosts(req, res);
+  }
+
+  @Post('addPost')
+  addPost(@Req() req, @Res() res) {
+    return this.appService.addPost(req, res);
   }
 }
