413074e979f8b5f11ff5742a4eccfb55b3398f20
th-release <cth.release@gmail.com>
2023-04-20 14:15:16 +0900

    style: App controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,39 +9,4 @@ export class AppController {
   getStatus() {
     return this.appService.getStatus();
   }
-
-  @Post('addQuest')
-  addQuest(@Req() req, @Res() res) {
-    return this.appService.addQuest(req, res);
-  }
-
-  @Post('addCategory')
-  addCategory(@Req() req, @Res() res) {
-    return this.appService.addCategory(req, res);
-  }
-
-  @Get('getQuest')
-  getQuest(@Req() req, @Res() res) {
-    return this.appService.getQuest(req, res);
-  }
-
-  @Get('getCategory')
-  getCategory(@Req() req, @Res() res) {
-    return this.appService.getCategory(req, res);
-  }
-
-  @Get('getPosts')
-  getPosts(@Req() req, @Res() res) {
-    return this.appService.getPosts(req, res);
-  }
-
-  @Post('delPost')
-  deletePosts(@Req() req, @Res() res) {
-    return this.appService.deletePosts(req, res);
-  }
-
-  @Post('addPost')
-  addPost(@Req() req, @Res() res) {
-    return this.appService.addPost(req, res);
-  }
 }
